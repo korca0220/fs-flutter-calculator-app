03b944dfd5475c147f03e4258c7b4ee133e2d764
ParkJuneWoo <krca0220@gmail.com>
2024-03-31 21:33:22 +0900

    패키지 분리

---
+++ app/junewoov/android/app/src/main/kotlin/com/myjunewoo/junewoov/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myjunewoo.junewoov
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/junewoov/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:calculator_domain_starter_ui/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const CalculatorApp());
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorHome(),
+    );
+  }
+}
+
+class CalculatorHome extends StatelessWidget {
+  const CalculatorHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Calculator Home'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ICalculatorRouteTo.calculator(context),
+      ),
+    );
+  }
+}
